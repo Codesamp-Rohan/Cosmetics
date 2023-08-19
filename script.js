86299ac4958f3e73c5d07da17cb6547d462fc65c
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-08-19 15:57:05 +0530

    Something new added

--- script.js
+++ script.js
@@ -22,23 +22,69 @@ overlay.addEventListener("click", function () {
   contactUs.classList.add("hidden");
   overlay.classList.add("hidden");
 });
-
-// const cursor = document.querySelector(".cursor");
-
-// window.addEventListener("mousemove", (e) => {
-//   setTimeout(() => {
-//     cursor.style.left = e.clientX + "px";
-//     cursor.style.top = e.clientY + "px";
-//   }, 10);
-// });
-
 // JS
 ScrollReveal().reveal(".navbar");
 ScrollReveal().reveal(".topAreaText", { delay: 300 });
 ScrollReveal().reveal(".aboutImage", { delay: 500 });
 ScrollReveal().reveal(".about>h1", { delay: 500 });
 
-ScrollReveal().reveal("#boxContent1", { delay: 300 });
-ScrollReveal().reveal("#boxContent2", { delay: 600 });
-ScrollReveal().reveal("#boxContent3", { delay: 900 });
-ScrollReveal().reveal("#boxContent4", { delay: 1200 });
+ScrollReveal().reveal("#boxContent1", { delay: 200 });
+ScrollReveal().reveal("#boxContent2", { delay: 400 });
+ScrollReveal().reveal("#boxContent3", { delay: 600 });
+ScrollReveal().reveal("#boxContent4", { delay: 800 });
+
+ScrollReveal().reveal(".boxImage1", { delay: 200 });
+ScrollReveal().reveal(".boxImage2", { delay: 400 });
+ScrollReveal().reveal(".boxImage3", { delay: 600 });
+ScrollReveal().reveal(".boxImage4", { delay: 800 });
+
+ScrollReveal().reveal(".rangeLine", { delay: 300 });
+
+$(".slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+});
+
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+
+const para1 = document.querySelector("#optionPara1");
+const para2 = document.querySelector("#optionPara2");
+const para3 = document.querySelector("#optionPara3");
+
+const leftProduct = document.querySelector(".leftProduct");
+
+const productImage1 = document.querySelector("#productImage1");
+const productImage2 = document.querySelector("#productImage2");
+const productImage3 = document.querySelector("#productImage3");
+
+option1.addEventListener("click", function () {
+  productImage1.classList.remove("hidden");
+  productImage2.classList.add("hidden");
+  productImage3.classList.add("hidden");
+  para1.style.fontWeight = "600";
+  para2.style.fontWeight = "100";
+  para3.style.fontWeight = "100";
+  leftProduct.style.marginTop = "0vh";
+});
+option2.addEventListener("click", function () {
+  productImage2.classList.remove("hidden");
+  productImage1.classList.add("hidden");
+  productImage3.classList.add("hidden");
+  para2.style.fontWeight = "600";
+  para1.style.fontWeight = "100";
+  para3.style.fontWeight = "100";
+  leftProduct.style.marginTop = "-15vh";
+});
+option3.addEventListener("click", function () {
+  productImage3.classList.remove("hidden");
+  productImage1.classList.add("hidden");
+  productImage2.classList.add("hidden");
+  para3.style.fontWeight = "600";
+  para1.style.fontWeight = "100";
+  para2.style.fontWeight = "100";
+  leftProduct.style.marginTop = "-30vh";
+});
