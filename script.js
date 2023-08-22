11e2f8e36e91db3e8421f9f2eca5bfd4818abcd4
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-08-22 09:41:53 +0530

    menu bar

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", function () {
+  loader.classList.add("hidden");
+});
+
 const dropbtn = document.querySelector(".dropbtn");
 const dropContent = document.querySelector(".dropdown-content");
 
@@ -103,3 +109,28 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+// Mobile Navbar
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s `;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+  //
+};
+
+navSlide();
